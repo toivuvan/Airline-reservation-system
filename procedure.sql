3a52c03aa60a489a1d66bb88b53a178df5b8a859
maitungdeptraiiiii <maiphananhtung@gmail.com>
2024-12-15 21:12:50 +0700

    thay doi file procedure

--- procedure.sql
+++ procedure.sql
@@ -1,15 +1,24 @@
 DELIMITER $$
 
-CREATE PROCEDURE CalculateTotalPayment(
-    IN customer_id INT,
-    OUT total_amount DECIMAL(10,2)
+CREATE PROCEDURE GetCustomerTickets(
+    IN customer_id INT
 )
 BEGIN
-    SELECT SUM(Payments.PaymentAmount)
-    INTO total_amount
-    FROM Payments
-    INNER JOIN Reservations ON Payments.ReservationID = Reservations.ReservationID
-    WHERE Reservations.CustomerID = customer_id;
+    SELECT t.TicketID, 
+           t.Class, 
+           t.TicketPrice, 
+           t.SeatNumber, 
+           fs.DepartureTime, 
+           fs.ArrivalTime, 
+           f.FlightNumber
+    FROM Tickets t
+    INNER JOIN Reservations r 
+        ON t.ReservationID = r.ReservationID
+    INNER JOIN FlightSchedules fs 
+        ON r.FlightSchedulesID = fs.FlightScheduleID
+    INNER JOIN Flights f 
+        ON fs.FlightID = f.FlightID
+    WHERE r.CustomerID = customer_id;
 END$$
 
 DELIMITER ;
